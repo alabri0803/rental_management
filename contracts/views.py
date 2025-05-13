2d5d6a45cd25058ccbe1da981185e08322d37651
alabri0803 <rashid92759346@gmail.com>
2025-05-13 15:28:51 +0000

    Pdf

--- contracts/views.py
+++ contracts/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contract
 from .forms import ContractForm
 from django.utils.translation import gettext_lazy as _
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
+def contract_pdf(request, pk):
+  contract = get_object_or_404(Contract, pk=pk)
+  template = get_template('contracts/contract_pdf.html')
+  context = {'contract': contract}
+  html = template.render(context)
+  response = HttpResponse(content_type='application/pdf')
+  response['Content-Disposition'] = f'attachment; filename="contract_{contract.id}.pdf"'
+  pisa.CreatePDF(html, dest=response)
+  return response
+      
 def contract_list(request):
   contracts = Contract.objects.all()
   return render(request, 'contracts/contract_list.html', {'contracts': contracts})
