93ef453b7009061afbb829adce9b640d7b4e192e
alabri0803 <rashid92759346@gmail.com>
2025-05-14 13:13:26 +0000

    urls

--- rental_management/urls.py
+++ rental_management/urls.py
@@ -17,12 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from core import views as core_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns, set_language
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += i18n_patterns(
     path('', core_views.dashboard, name='dashboard'),
     path('contracts/', include('contracts.urls')),
     path('tenants/', include('tenants.urls')),
     path('payments/', include('payments.urls')),
     path('reports/', include('reports.urls')),
     path('api/', include('api.urls')),
-]
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('set-language/', set_language, name='set_language'),
+    prefix_default_language=False,
+)
