70585d392dafd9f9461e9abeb626904f70f7ce66
alabri0803 <rashid92759346@gmail.com>
2025-05-15 17:51:34 +0000

    set_language

--- rental_management/urls.py
+++ rental_management/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +30,7 @@ urlpatterns = [
     path('buildings/', include('buildings.urls')),
     path('reports/', include('reports.urls')),
     path('api/', include('api.urls')),
+    path('i18n/', include('django.conf.urls.i18n'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
