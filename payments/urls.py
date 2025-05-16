37e278aa6c750a46808d9b260a717101609c55a7
alabri0803 <rashid92759346@gmail.com>
2025-05-16 16:46:43 +0000

    Detail form list payment

--- payments/urls.py
+++ payments/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'payments'
 
 urlpatterns = [
-  path('', views.payment_list, name='payment_list'),
-  path('add/', views.payment_create, name='payment_create'),
-  path('<int:pk>/', views.payment_detail, name='payment_detail'),
-  path('<int:pk>/edit/', views.payment_update, name='payment_update'),
+  path('', views.payment_list, name='list'),
+  path('add/', views.payment_create, name='add'),
+  path('<int:pk>/', views.payment_detail, name='detail'),
+  path('<int:pk>/edit/', views.payment_update, name='edit'),
 ]
