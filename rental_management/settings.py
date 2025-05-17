15335f543c2be0399e891b36d96bcd33130d9e36
alabri0803 <rashid92759346@gmail.com>
2025-05-17 10:31:48 +0000

    Jv

--- rental_management/settings.py
+++ rental_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'modeltranslation',
     'buildings',
     'tenants',
     'contracts',
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
